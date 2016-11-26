29cc12b4fac6b79ceff0f7d50cf31a58e62bac55
jgarcia77 <jgarcia773@gmail.com>
2016-11-26 12:11:29 -0600

    Social media links

--- WebSite/Models/Helper.cs
+++ WebSite/Models/Helper.cs
@@ -3,5 +3,7 @@
     public static class Helper
     {
         public static string GoogleDirectionsUrl = "https://www.google.com/maps/dir/''/hilton+oak+brook+hills+resort+in+oak+brook+il/@41.8212263,-88.0420511,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x880e4e80128e97e7:0x7907206e6fd07ffd!2m2!1d-87.9720112!2d41.8212476";
+
+        public static string GoogleDirectionsUrlMobile = "https://maps.google.com/maps?daddr=3500 Midwest Rd, Oak Brook, IL 60523";
     }
 }
